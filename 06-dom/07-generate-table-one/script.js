604523db4b2c26c38d00197569d703794a38f7c3
Beardificent <cis.magito@gmail.com>
2020-08-28 14:36:25 +0200

    done

---
+++ 06-dom/07-generate-table-one/script.js
@@ -0,0 +1,20 @@
+/* becode/javascript
+ *
+ * /06-dom/07-generate-table-one/script.js - 6.7: génération d'un tableau (1)
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+//THANKS HENDRIK FOR THIS EASY WAY TO MAKE A TABLE
+
+
+(function() {
+    // create elements <table> and a <tbody>
+    document.getElementById("target").innerHTML = "<table><tr>1</tr><tr>2</tr><tr>3</tr><tr>4</tr><tr>5</tr><tr>6</tr><tr>7</tr><tr>8</tr><tr>9</tr><tr>10</tr></table>";
+})();
+
+
