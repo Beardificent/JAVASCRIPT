31e72cc4a87c319560c2cdf50783040c33fe2248
Beardificent <cis.magito@gmail.com>
2020-08-31 09:27:03 +0200

    done

---
+++ 06-dom/09-match-password-one/script.js
@@ -0,0 +1,30 @@
+/* becode/javascript
+ *
+ * /06-dom/09-match-password-one/script.js - 6.9: vérification de mots de passe (1)
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+document.getElementById("run").addEventListener("click", function (){
+    checkInput();
+    if (left.value === right.value) {
+        left.style.backgroundColor = "";
+        right.style.backgroundColor = "";
+    } else {
+        left.style.backgroundColor = "red";
+        right.style.backgroundColor = "red";
+    }
+})
+
+    function checkInput(){
+        left = document.getElementById("pass-one");
+        right = document.getElementById("pass-two");
+    }
+
+})();
+
