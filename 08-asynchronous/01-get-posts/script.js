6f9734f06797368f0f27c3773212dbbc3a7ea870
Beardificent <cis.magito@gmail.com>
2020-09-07 10:55:02 +0200

    i think this is done

--- 08-asynchronous/01-get-posts/script.js
+++ 08-asynchronous/01-get-posts/script.js
@@ -9,19 +9,16 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-let error = "Error";
-let response = "Succes";
+(function getPosts () {
 
-window.lib.getPosts(error, response)
-{
-    if(error){
-        console.log(error);
-    }
-    else {
-       console.log(response);
-    }
-};
-
-window.lib.getPosts();
-
-/* */
+    document.getElementById('run').addEventListener("click", ()=> {
+        window.lib.getPosts((reject, response)=> {
+            if (reject) {
+                console.log(1);
+            } else {
+                console.log(null);
+                console.log(response);
+            }
+        });
+    })
+})();
