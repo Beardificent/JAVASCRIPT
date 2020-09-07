98dae3a22d865e681e05ca799993c272d5fb9dc9
Beardificent <cis.magito@gmail.com>
2020-09-07 16:11:34 +0200

    done with class and alone

--- 08-asynchronous/02-get-comments/script.js
+++ 08-asynchronous/02-get-comments/script.js
@@ -9,6 +9,49 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(() => {
-    // your code here
+
+(function () {
+    document.getElementById('run').addEventListener('click', function () {
+        window.lib.getPosts((reject, article) => {
+            if (reject) {
+                console.log(reject);
+            } else {
+                //call getComments for each article
+                article.forEach(getArticle => {
+                    window.lib.getComments(getArticle.id, (reject, table) => {
+                        if (reject) {
+                            console.log(reject)
+                        } else {
+                            getArticle.id = table;
+                            console.log(table);
+                        }
+                    });
+                })
+            }
+        });
+    })
 })();
+
+//SICCOS
+/*
+
+let run = document.getElementById('run')
+run.addEventListener('click', getPostsWithComments)
+
+function getPostsWithComments (){
+    window.lib.getPosts(function(error, posts){
+        if (error){
+            console.log(error)
+        }else {
+            posts.forEach(post => {
+                window.lib.getComments(post.id, function (error, comments) {
+                    if (error) {
+                        console.error(error);
+                    }
+                })
+            })
+            console.log(post);
+
+
+ */
+
