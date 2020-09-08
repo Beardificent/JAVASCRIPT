8b3b3777df8a891c05203916c67736a5ab14c25b
Beardificent <cis.magito@gmail.com>
2020-09-08 12:02:07 +0200

    9.2 source to answer documented as comment

--- 09-fetch/02-list-to-template/script.js
+++ 09-fetch/02-list-to-template/script.js
@@ -9,6 +9,29 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+//SOURCE : https://howtocreateapps.com/fetch-and-display-json-html-javascript/
+
 (() => {
-    // your code here
+    let run = document.getElementById('run');
+
+    run.addEventListener('click', function (){
+
+        fetch("http://localhost:3000/heroes")
+            .then(response => response.json())
+            .then(data => appendData(data))
+        function appendData(data){
+            console.log(data);
+            let target = document.getElementById('target');
+            for (let i = 0; i < data.length; i++){
+                let div = document.createElement("div");
+                div.innerHTML = '<h4 class="title">\n' +
+                    '<strong class="name">' + data[i].name + '</strong>\n' +
+                    '<em class="alter-ego">' + data[i].alterEgo + '</em>\n' +
+                    '</h4>\n' +
+                    '<p class="powers">' + data[i].powers + '</p>'
+                target.appendChild(div);
+            }
+        }
+    })
 })();
+
