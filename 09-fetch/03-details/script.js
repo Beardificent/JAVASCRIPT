e0cf33f6ada2c9530b033b666ba94160c2e094fb
Beardificent <cis.magito@gmail.com>
2020-09-09 10:08:26 +0200

    edited my code with code from 9.2

--- 09-fetch/03-details/script.js
+++ 09-fetch/03-details/script.js
@@ -9,6 +9,34 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+//MOST OF THIS CODE IS FROM 9.2 EXCEPT INPUT + CHECKINPUT
+//Same exercise except we need to identify which hero value has been asked in input field
+
+
 (() => {
-    // your code here
+    let target = document.querySelector('#target');
+
+    let getHero = () => {
+        let Input = document.querySelector('#hero-id').value;
+
+        // fetch the data from the server
+        fetch('http://localhost:3000/heroes')
+            .then(respond => {
+                return respond.json();
+            }).then(allData => {
+            allData.forEach(data => {
+
+                if (data.id == Input) {
+                    let tempVar = document.querySelector("#tpl-hero").content.cloneNode(true);
+                    tempVar.querySelector(".name").innerHTML = data.name;
+                    tempVar.querySelector(".alter-ego").innerHTML = data.alterEgo;
+                    tempVar.querySelector(".powers").innerHTML = data.abilities;
+                    target.appendChild(tempVar);
+
+                }
+            })
+        })
+    };
+    document.querySelector('#run').addEventListener('click', getHero)
 })();
+
